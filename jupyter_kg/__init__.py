e7568b0999180113aa97b4d98a3348baaeee589d
René Fritze <rene.fritze@wwu.de>
2023-01-17 11:49:51 +0100

    setup infra

---
+++ jupyter_kg/__init__.py
@@ -0,0 +1,20 @@
+import ipycytoscape as ipycytoscape
+import rdflib.graph
+
+
+def parse_graph(turtle_contents:str) -> rdflib.graph.Graph:
+    g = rdflib.graph.Graph()
+    g.parse(data=turtle_contents, format="turtle")
+    return g
+
+
+def run_sparql_query(graph:rdflib.graph.Graph, query:str) -> list:
+    return graph.query(query).bindings
+
+
+def visualize_graph(graph:rdflib.graph.Graph) -> ipycytoscape.CytoscapeWidget:
+    cytoscape_obj = ipycytoscape.CytoscapeWidget()
+    from rdflib.extras.external_graph_libs import rdflib_to_networkx_digraph
+    nx = rdflib_to_networkx_digraph(graph)
+    cytoscape_obj.graph.add_graph_from_networkx(nx)
+    return cytoscape_obj
